9aecd66e66b1b3a6e57de6110112756111571936
jakebostic <jake_bostic@yahoo.com>
2019-01-31 15:20:32 -0500

    adding movie rest controller

--- exercises/movie-service/src/main/java/microservice/workshop/movieservice/http/MovieController.java
+++ exercises/movie-service/src/main/java/microservice/workshop/movieservice/http/MovieController.java
@@ -1,5 +1,17 @@
 package microservice.workshop.movieservice.http;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import microservice.workshop.movieservice.data.MovieRepository;
+import microservice.workshop.movieservice.model.Movie;
+
 /*
  * TODO (Exercise 2) - Make this a proper rest controller.
  * 
@@ -7,6 +19,14 @@ package microservice.workshop.movieservice.http;
  * If the movie exists, return the movie and status OK (200).
  * If the movie does not exist, return a status of NOT_FOUND (404)
  */
+@RestController
+@RequestMapping("/movie")
 public class MovieController {
+	
+	@Autowired MovieRepository movieRepository;
+	
+	public @ResponseBody Optional<Movie> getMovie(@RequestParam int id) {
+		return movieRepository.findById(id);
+	}
 
 }
