03b3a95e830a56a364afdf011e427f07527a069c
jakebostic <jake_bostic@yahoo.com>
2019-01-31 13:13:20 -0500

    initial commit

--- exercises/movie-cast-service/src/main/java/microservice/workshop/moviecastservice/http/CastController.java
+++ exercises/movie-cast-service/src/main/java/microservice/workshop/moviecastservice/http/CastController.java
@@ -1,5 +1,17 @@
 package microservice.workshop.moviecastservice.http;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import microservice.workshop.moviecastservice.data.CastMemberRepository;
+import microservice.workshop.moviecastservice.model.CastMember;
+
 /*
  * TODO (Exercise 2) - Make this a proper rest controller.
  * 
@@ -7,5 +19,15 @@ package microservice.workshop.moviecastservice.http;
  * The controller should always return a list of cast members - an empty list is a valid response if
  * there are no cast members for the given ID.
  */
+
+@RestController
+@RequestMapping("/cast")
 public class CastController {
+	
+	@Autowired CastMemberRepository castRepository;
+	
+	@GetMapping(path="/search")
+	public @ResponseBody List<CastMember> getCastMembers(@RequestParam int movieId) {
+		return castRepository.findByMovieId(movieId);
+	}
 }
