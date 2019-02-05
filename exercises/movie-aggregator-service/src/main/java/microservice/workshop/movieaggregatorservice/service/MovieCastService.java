b450308200e0fa9a3284ef380127af06df1acd61
jakebostic <jake_bostic@yahoo.com>
2019-02-05 12:05:25 -0500

    added Get mapping

--- exercises/movie-aggregator-service/src/main/java/microservice/workshop/movieaggregatorservice/service/MovieCastService.java
+++ exercises/movie-aggregator-service/src/main/java/microservice/workshop/movieaggregatorservice/service/MovieCastService.java
@@ -3,6 +3,7 @@ package microservice.workshop.movieaggregatorservice.service;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import microservice.workshop.movieaggregatorservice.model.CastMember;
@@ -10,6 +11,7 @@ import microservice.workshop.movieaggregatorservice.model.CastMember;
 @FeignClient(name="movie-cast-service", url="http://localhost:8082")
 public interface MovieCastService {
 	
+	@GetMapping("/cast/search")
 	List<CastMember> getCastMembers(@RequestParam("movieId") Integer movieId);
 
 }
